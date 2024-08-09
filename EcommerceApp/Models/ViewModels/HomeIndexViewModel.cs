161b557b4057761cc2ef1442afd7942c2d9c7bc2
mertcekuc <cekucmert@gmail.com>
2024-08-09 18:05:28 +0300

    New pages

---
+++ EcommerceApp/Models/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceApp.Models;
+
+namespace EcommerceApp.Models.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<Product> Products { get; set; }
+        
+        public List<Category> Categories { get; set; }
+
+    }
+}
